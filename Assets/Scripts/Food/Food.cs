df3c0d958ac3dd3060a863c70c5e3521c34f6b94
Stephen Hubbard <hubbard_stephen@yahoo.com>
2023-05-23 17:43:56 -0600

    Monsters can't eat food until they land
    Can still collide with other portals fwiw but I think it's actualyl pretty funny and people might enjoy it

--- Assets/Scripts/Food/Food.cs
+++ Assets/Scripts/Food/Food.cs
@@ -1,14 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Food : MonoBehaviour
 {
     public enum FoodType { Cow, Chicken, Pig, Human };
 
+    const string MONSTER_STRING = "Monster";
+
     [SerializeField] private FoodType _foodType;
+
+    private Collider2D _col;
+
+    private void Awake() {
+        _col = GetComponent<Collider2D>();
+    }
 
     public FoodType GetFoodType() {
         return _foodType;
+    }
+
+    private void Start()
+    {
+        ToggleFriendlyCollider(true);
+    }
+
+    public void ToggleFriendlyCollider(bool value)
+    {
+        foreach (Monster monster in FindObjectsOfType<Monster>())
+        {
+            if (monster.gameObject.layer == LayerMask.NameToLayer(MONSTER_STRING))
+            {
+                Collider2D otherCollider = monster.GetComponent<Collider2D>();
+
+                if (otherCollider != null && !monster.HasLanded)
+                {
+                    Physics2D.IgnoreCollision(_col, otherCollider, value);
+                }
+            }
+        }
     }
 }
